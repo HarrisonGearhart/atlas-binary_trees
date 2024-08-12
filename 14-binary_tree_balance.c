5e9def6fffa7ab4a63ad0542152a77ba033ae550
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-08-12 16:54:25 -0500

    file_14/ add height calculation

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,3 +10,26 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
+/**
+ * binary_tree_height - measures height of the tree
+ * @tree: tree being measured
+ * Return: height of tree or 0 if null
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
+}
