92cfcca99e4deb48f0a9381720a794ab7e428324
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-08-12 15:48:59 -0500

    file_5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -10,5 +10,10 @@ int binary_tree_is_root(const binary_tree_t *node)
 	{	
 		return (0);
 	}	
-	return (node->parent == NULL);
+	if (node->parent == NULL);
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
