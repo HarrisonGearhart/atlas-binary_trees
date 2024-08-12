1335317f0c2f0b2326c99e01b6fc12863ed84dd0
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-08-12 17:40:00 -0500

    file_17/betty

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_sibling - finds the sibling of a node
  * @node: pointer to node to find
- * Return: sibling or NULL if NULL 
+ * Return: sibling or NULL if NULL
 */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
