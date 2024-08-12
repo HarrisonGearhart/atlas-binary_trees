e8cc76bca32893d486d8d9165f36a7550e378421
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-08-12 16:34:49 -0500

    file_13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,6 +9,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
-		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right) + 1);
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
 	return (0);
 }
